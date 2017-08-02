18a771c987551af422e407a35dfdc2659941180b
Rob Blake <robertb@property24.com>
2017-08-02 13:29:36 +0200

    Commented out existing unit tests (for now).

--- Code/PokerHandExercise.Tests/Tests/PokerHandComparerTests.cs
+++ Code/PokerHandExercise.Tests/Tests/PokerHandComparerTests.cs
@@ -15,27 +15,27 @@ namespace PokerHandExercise.Tests.Tests
       _comparer = new PokerHandComparer();
     }
 
-    [TestMethod]
-    public void HighStraightFlushVsLowStraightFlush()
-    {
-      var pokerHand1 = PokerHandTestHelper.CreateHighStraightFlush();
-      var pokerHand2 = PokerHandTestHelper.CreateAceLowStraightFlush();
+    //[TestMethod]
+    //public void HighStraightFlushVsLowStraightFlush()
+    //{
+    //  var pokerHand1 = PokerHandTestHelper.CreateHighStraightFlush();
+    //  var pokerHand2 = PokerHandTestHelper.CreateAceLowStraightFlush();
 
-      var result = _comparer.CompareHands(pokerHand1, pokerHand2);
-      Assert.AreEqual(1, result, "Expected Hand2 to lose to Hand1");
-      result = _comparer.CompareHands(pokerHand2, pokerHand1);
-      Assert.AreEqual(-1, result, "Expected Hand1 to beat Hand2");
-    }
+    //  var result = _comparer.CompareHands(pokerHand1, pokerHand2);
+    //  Assert.AreEqual(1, result, "Expected Hand2 to lose to Hand1");
+    //  result = _comparer.CompareHands(pokerHand2, pokerHand1);
+    //  Assert.AreEqual(-1, result, "Expected Hand1 to beat Hand2");
+    //}
 
-    [TestMethod]
-    public void MatchingStraightFlushes()
-    {
-      var pokerHand1 = PokerHandTestHelper.CreateAceLowStraightFlush();
-      var pokerHand2 = PokerHandTestHelper.CreateAceLowStraightFlush();
+    //[TestMethod]
+    //public void MatchingStraightFlushes()
+    //{
+    //  var pokerHand1 = PokerHandTestHelper.CreateAceLowStraightFlush();
+    //  var pokerHand2 = PokerHandTestHelper.CreateAceLowStraightFlush();
 
-      var result = _comparer.CompareHands(pokerHand1, pokerHand2);
-      Assert.AreEqual(0, result, "Expected Hand1 and Hand2 be equivalent");
-    }
+    //  var result = _comparer.CompareHands(pokerHand1, pokerHand2);
+    //  Assert.AreEqual(0, result, "Expected Hand1 and Hand2 be equivalent");
+    //}
 
   }
 }
