5f73513906c1164653a3801b44d4d4796e24d027
Rob Blake <robertb@property24.com>
2017-08-02 13:48:14 +0200

    Start own initial test on the newly created PokerHandFactory.

---
+++ Code/PokerHandExercise.Tests/Tests/PokerHandFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHandExercise.Tests.Classes;
+using PokerHandExercise.Classes;
+using PokerHandExercise.Classes.HandFactory;
+using PokerHandExercise.Classes.Hands;
+
+namespace PokerHandExercise.Tests.Tests
+{
+    [TestClass]
+    public class PokerHandFactoryTests
+    {
+        [TestMethod]
+        public void Factory_WhenPassed_A_StraightFlush_Combination_Returns_A_StraightFlush()
+        {
+            PokerHand pokerHand = PokerHandTestHelper.CreateHighStraightFlush();
+            PokerHandFactory pokerHandFactory = new PokerHandFactory();
+            SpecifiedPokerHand specifiedPokerHand = pokerHandFactory.Create(pokerHand);
+
+            Assert.IsTrue(specifiedPokerHand is StraightFlush);
+        }
+    }
+
+
+}
---
+++ Code/PokerHandExercise/Classes/HandFactory/PokerHandFactory.cs
@@ -0,0 +1,17 @@
+﻿using PokerHandExercise.Classes.Hands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandExercise.Classes.HandFactory
+{
+    internal class PokerHandFactory
+    {
+        internal SpecifiedPokerHand Create(PokerHand pokerHand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
